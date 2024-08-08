a10a0deb132b79dfc8bfde8f3538d56b82d194ac
bemly_ <2328712329@qq.com>
2024-08-09 01:40:20 +0800

    upload document

---
+++ docs/besh/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["main"],"mod":["better_truth_tty","common_shell","error"]};
---
+++ docs/src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["besh",["",[],["better_truth_tty.rs","common_shell.rs","error.rs","main.rs"]]],\
+["libc",["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]]]\
+]'));
+createSrcSidebar();
